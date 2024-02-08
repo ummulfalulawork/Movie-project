0b4273c010c14ceab3e6276d98babecf04bd076e
ummulfalulawork <ummulfalulawork@gmail.com>
2024-02-08 10:53:21 +0530

    added

--- movieproject/urls.py
+++ movieproject/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("movieapp.urls"))
